d8c5d8b6635c5038e58ccb4b1e77e47ee5977819
Sara Sinisterra Villota <sinisterra.sara@correounivalle.edu.co>
2025-08-02 21:43:40 -0500

    comprobar algo

--- src/components/Carta.vue
+++ src/components/Carta.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <h2 class="text-4xl font-bold mb-4">¡Feliz cumpleaños!</h2>
-        <p class="text-lg">Espero que tu día esté lleno de alegría y sorpresas.</p>
+        <p class="text-lg">Espero que tu día esté lleno de alegría y sorpresas, te amo.</p>
     </div>
 </template>
 <script>
